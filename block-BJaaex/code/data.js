c72704cdfda908001d0fd7157d9f042772816f5d
Reena Mendonsa <reenamendonsa@gmail.com>
2021-08-11 12:01:52 +0530

    Memory Game

---
+++ block-BJaaex/code/data.js
@@ -0,0 +1,35 @@
+const cardsArray = [
+    {
+      name: 'diamond',
+      icon: '<i class="fa fa-diamond"></i>',
+    },
+    {
+      name:'plane',
+      icon:'<i class="fa fa-paper-plane-o"></i>',
+    },
+    {
+        name:'spider',
+        icon:'<i class="fas fa-spider"></i>',
+    },
+    {
+        name:'medal',
+        icon:'<i class="fas fa-medal"></i>',
+    },
+    {
+        name:'anchor',
+        icon:'<i class="fa fa-anchor"></i>',
+    },
+    {
+        name:'pizza',
+        icon:'<i class="fas fa-pizza-slice"></i>',
+    },
+    {
+        name:'cycle',
+        icon:'<i class="fa fa-bicycle"></i>',
+    },
+    {
+        name:'apple',
+        icon:'<i class="fas fa-apple-alt"></i>',
+    },
+
+]   
